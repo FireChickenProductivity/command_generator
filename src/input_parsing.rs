c1f080b071df452b105a975024b9fc873a4972bb
Fire Chicken <firechickenproductivity@gmail.com>
2025-05-15 15:57:31 -0600

    Simplify code a little

--- src/input_parsing.rs
+++ src/input_parsing.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::fs::File;
 
 pub fn get_file_from_user() -> File {
-	let file = loop {
+	return loop {
 		println!("Input the filepath to the command record: ");
 		let mut input = String::new();
 		let _result = io::stdin().read_line(&mut input);
@@ -10,20 +10,13 @@ pub fn get_file_from_user() -> File {
 			Ok(_result) => {
 				let _file_result = File::open(input.trim());
 				match _file_result {
-					Ok(valid_file) => {
-						break valid_file;
-					}
-					Err(_) => {
-						println!("Please input a valid path.");
-					}
+					Ok(valid_file) => break valid_file,
+					Err(_) => println!("Please input a valid path."),
 				}
 			}
-			Err(_) => {
-				println!("Error reading input!");
-			}
+			Err(_) => println!("Error reading input!"),
 		}
 	};
-	file
 }
 
 fn get_nonnegative_integer_from_user(prompt: &str, default: u32) -> u32 {
@@ -42,9 +35,7 @@ fn get_nonnegative_integer_from_user(prompt: &str, default: u32) -> u32 {
 					println!("Please enter a non-negative integer.");
 				}
 			}
-			Err(_) => {
-				println!("Error reading input!");
-			}
+			Err(_) => println!("Error reading input!"),
 		}
 	}
 }
