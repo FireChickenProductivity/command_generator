75a368b507d8aedde2a6708228b1ac661a733baa
Fire Chicken <firechickenproductivity@gmail.com>
2025-05-15 14:40:19 -0600

    Get the other arguments

--- src/input_parsing.rs
+++ src/input_parsing.rs
@@ -14,7 +14,7 @@ pub fn get_file_from_user() -> File {
 						break valid_file;
 					}
 					Err(_) => {
-						println!("Received invalid filepath!");
+						println!("Please input a valid path.");
 					}
 				}
 			}
@@ -26,7 +26,7 @@ pub fn get_file_from_user() -> File {
 	file
 }
 
-pub fn get_nonnegative_integer_from_user(prompt: &str, default: u32) -> u32 {
+fn get_nonnegative_integer_from_user(prompt: &str, default: u32) -> u32 {
 	loop {
 		println!("{}", prompt);
 		let mut input = String::new();
@@ -47,4 +47,12 @@ pub fn get_nonnegative_integer_from_user(prompt: &str, default: u32) -> u32 {
 			}
 		}
 	}
+}
+
+pub fn get_max_chain_size_from_user() -> u32 {
+	get_nonnegative_integer_from_user("Input the maximum number of consecutive commands to consider as a single potential command.\nMaking this bigger can allow finding longer patterns but it takes longer. Press enter with no input to take default of 20: ", 20)
+}
+
+pub fn get_number_of_recommendations_from_user() -> u32 {
+	get_nonnegative_integer_from_user("Input the maximum number of command recommendations to output. Press enter with no input to take default of 0: ", 0)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod input_parsing;
 fn main() {
 	let _file = input_parsing::get_file_from_user();
 	println!("read file");
-	let number = input_parsing::get_nonnegative_integer_from_user("Input the command chain size: ", 20);
-	println!("read number {number}");
+	let chain_size = input_parsing::get_max_chain_size_from_user();
+	println!("read number {chain_size}");
+	let number_of_recommendations = input_parsing::get_number_of_recommendations_from_user();
+	println!("read number {number_of_recommendations}");
 } 
