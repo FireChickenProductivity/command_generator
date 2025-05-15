b1bcf5c1d430f03def2fdbedf67eb871d44cd01a
Fire Chicken <firechickenproductivity@gmail.com>
2025-05-15 17:15:43 -0600

    Use command line arguments

--- src/input_parsing.rs
+++ src/input_parsing.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::fs::File;
+use std::env;
 
 pub fn get_file_from_user() -> File {
 	return loop {
@@ -54,10 +55,44 @@ pub struct InputParameters {
 	pub number_of_recommendations: u32,
 }
 
+fn get_file(arguments: &Vec<String>) -> File {
+	if arguments.len() < 2 {
+		get_file_from_user()
+	} else if let Ok(file) = File::open(&arguments[1]) {
+		file
+	} else {
+		println!("Could not open the record file.");
+		get_file_from_user()
+	}
+}
+
+fn get_max_chain_size(arguments: &Vec<String>) -> u32 {
+	if arguments.len() < 3 {
+		get_max_chain_size_from_user()
+	} else if let Ok(size) = arguments[2].parse() {
+			size
+	} else {
+		println!("Could not parse the maximum chain size.");
+		get_max_chain_size_from_user()
+	}
+}
+
+fn get_number_of_recommendations(arguments: &Vec<String>) -> u32 {
+	if arguments.len() < 4 {
+		get_number_of_recommendations_from_user()
+	} else if let Ok(size) = arguments[3].parse() {
+			size
+	} else {
+		println!("Could not parse the number of recommendations.");
+		get_number_of_recommendations_from_user()
+	}
+}
+
 pub fn get_input_parameters_from_user() -> InputParameters {
-	let record_file = get_file_from_user();
-	let max_chain_size = get_max_chain_size_from_user();
-	let number_of_recommendations = get_number_of_recommendations_from_user();
+	let arguments: Vec<String> = env::args().collect();
+	let record_file = get_file(&arguments);
+	let max_chain_size = get_max_chain_size(&arguments);
+	let number_of_recommendations = get_number_of_recommendations(&arguments);
 
 	InputParameters {
 		record_file,
