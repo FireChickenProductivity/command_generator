9d630b1c9800d62ebcf5d4fb191c88a4b20a0d1b
Fire Chicken <firechickenproductivity@gmail.com>
2025-05-22 21:29:37 -0600

    Make some stuff private

--- src/input_parsing.rs
+++ src/input_parsing.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::fs::File;
 use std::env;
 
-pub fn get_file_from_user() -> File {
+fn get_file_from_user() -> File {
 	return loop {
 		println!("Input the filepath to the command record: ");
 		let mut input = String::new();
@@ -41,11 +41,11 @@ fn get_nonnegative_integer_from_user(prompt: &str, default: u32) -> u32 {
 	}
 }
 
-pub fn get_max_chain_size_from_user() -> u32 {
+fn get_max_chain_size_from_user() -> u32 {
 	get_nonnegative_integer_from_user("Input the maximum number of consecutive commands to consider as a single potential command.\nMaking this bigger can allow finding longer patterns but it takes longer. Press enter with no input to take default of 20: ", 20)
 }
 
-pub fn get_number_of_recommendations_from_user() -> u32 {
+fn get_number_of_recommendations_from_user() -> u32 {
 	get_nonnegative_integer_from_user("Input the maximum number of command recommendations to output. Press enter with no input to take default of 0: ", 0)
 }
 
