a3a580917a9843b484d1798e4e698c2c0c3314dc
Fire Chicken <firechickenproductivity@gmail.com>
2025-05-25 17:22:04 -0600

    Make instance variables private

--- src/action_records.rs
+++ src/action_records.rs
@@ -1,58 +1,73 @@
 /*
 class BasicAction:
-    def __init__(self, name, arguments):
-        self.name = name
-        self.arguments = arguments
-    
-    def compute_talon_script(self):
-        code = self.name + '(' + ', '.join(self.compute_arguments_converted_to_talon_script_string()) + ')'
-        return code
-    
-    def compute_arguments_converted_to_talon_script_string(self):
-        result = []
-        for argument in self.arguments:
-            if type(argument) == str:
-                converted_argument = self.compute_string_argument(argument)
-            elif type(argument) == bool:
-                converted_argument = str(compute_talon_script_boolean_value(argument))
-            else:
-                converted_argument = str(argument)
-            result.append(converted_argument)
-        return result
-    
-    def compute_string_argument(self, argument: str):
-        string_argument = "'" + argument.replace("'", "\\'") + "'"
-        return string_argument
-    
-    def get_name(self):
-        return self.name
-    
-    def get_arguments(self):
-        return self.arguments
-    
-    def to_json(self) -> str:
-        return json.dumps({'name': self.name, 'arguments': self.arguments}, cls = BasicActionEncoder)
-    
-    @staticmethod
-    def from_json(text: str):
-        representation = json.loads(text)
-        return BasicAction(representation['name'], representation['arguments'])
-    
-    def __eq__(self, other) -> bool:
-        return other is not None and self.name == other.name and self.arguments == other.arguments
-    
-    def __repr__(self):
-        return self.__str__()
-    
-    def __str__(self):
-        return self.to_json() */
+	def __init__(self, name, arguments):
+		self.name = name
+		self.arguments = arguments
+	
+	def compute_talon_script(self):
+		code = self.name + '(' + ', '.join(self.compute_arguments_converted_to_talon_script_string()) + ')'
+		return code
+	
+	def compute_arguments_converted_to_talon_script_string(self):
+		result = []
+		for argument in self.arguments:
+			if type(argument) == str:
+				converted_argument = self.compute_string_argument(argument)
+			elif type(argument) == bool:
+				converted_argument = str(compute_talon_script_boolean_value(argument))
+			else:
+				converted_argument = str(argument)
+			result.append(converted_argument)
+		return result
+	
+	def compute_string_argument(self, argument: str):
+		string_argument = "'" + argument.replace("'", "\\'") + "'"
+		return string_argument
+	
+	def get_name(self):
+		return self.name
+	
+	def get_arguments(self):
+		return self.arguments
+	
+	def to_json(self) -> str:
+		return json.dumps({'name': self.name, 'arguments': self.arguments}, cls = BasicActionEncoder)
+	
+	@staticmethod
+	def from_json(text: str):
+		representation = json.loads(text)
+		return BasicAction(representation['name'], representation['arguments'])
+	
+	def __eq__(self, other) -> bool:
+		return other is not None and self.name == other.name and self.arguments == other.arguments
+	
+	def __repr__(self):
+		return self.__str__()
+	
+	def __str__(self):
+		return self.to_json() */
 
 pub struct BasicAction {
-	pub name: String,
-	pub arguments: Vec<String>,
+	name: String,
+	arguments: Vec<String>,
 }
 
 impl BasicAction {
+	pub fn new(name: &str, arguments: Vec<String>) -> Self {
+		BasicAction {
+			name: String::from("name"),
+			arguments,
+		}
+	}
+
+	pub fn get_name(&self) -> &str {
+		&self.name
+	}
+
+	pub fn get_arguments(&self) -> &Vec<String> {
+		&self.arguments
+	}
+
 	pub fn compute_talon_script(&self) -> String {
 		let arguments_text = self.compute_arguments_converted_to_talon_script_string()
 			.join(", ");
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@ mod input_parsing;
 mod action_records;
 
 fn main() {
-	let action = action_records::BasicAction {
-		name: String::from("test"),
-		arguments: vec![String::from("arg1"), String::from("arg2")],
-	};
+	let action = action_records::BasicAction::new ("test", vec![String::from("arg1"), String::from("arg2")],
+	);
 	println!("Action: {}", action.compute_talon_script());
 	println!("json: {}", action.to_json());
-
 	let parameters = input_parsing::get_input_parameters_from_user();
 	println!("read number {}", parameters.max_chain_size);
 	println!("read number {}", parameters.number_of_recommendations);
