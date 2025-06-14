d196b79d32f306865886c8742899b239ac5868d4
Fire Chicken <firechickenproductivity@gmail.com>
2025-06-14 16:46:05 -0600

    Reformat main

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,66 @@
-mod input_parsing;
 mod action_records;
-mod recommendation_generation;
 mod action_utilities;
-mod text_separation;
 mod data_output;
+mod input_parsing;
 mod pool;
+mod recommendation_generation;
+mod text_separation;
 
-use action_records::{BasicAction, Argument, Command, read_file_record, Entry};
-use recommendation_generation::{compute_recommendations_from_record, PotentialCommandInformation, create_sorted_info};
-use std::time::Instant;
+use action_records::{Argument, BasicAction, Command, Entry, read_file_record};
 use data_output::{create_data_directory, output_recommendations};
+use recommendation_generation::{
+    PotentialCommandInformation, compute_recommendations_from_record, create_sorted_info,
+};
+use std::time::Instant;
 
 fn print_record(record: Result<Vec<Entry>, String>) {
-	match record {
-		Ok(record) => {
-			for entry in record {
-				match entry {
-					Entry::RecordingStart => println!("Recording started."),
-					Entry::Command(command) => {
-						println!("Command: {}", command.to_string());
-					}
-				}
-			}
-		},
-		Err(e) => println!("Error reading record file:\n	{}", e),
-	}
+    match record {
+        Ok(record) => {
+            for entry in record {
+                match entry {
+                    Entry::RecordingStart => println!("Recording started."),
+                    Entry::Command(command) => {
+                        println!("Command: {}", command.to_string());
+                    }
+                }
+            }
+        }
+        Err(e) => println!("Error reading record file:\n	{}", e),
+    }
 }
 
-
-
 fn main() {
-	match create_data_directory() {
-		Ok(_) => {}
-		Err(e) => {
-			println!("Error creating data directory: {}", e);
-			return;
-		}
-	}
-	let parameters = input_parsing::get_input_parameters_from_user();
-	let start_time = Instant::now();
-	println!("Reading file");
-	let record = read_file_record(parameters.record_file);
-	match record {
-		Ok(record) => {
-			println!("Generating recommendations");
-			let recommendations = compute_recommendations_from_record(&record, parameters.max_chain_size);
-			let elapsed_time = start_time.elapsed();
-			let output = create_sorted_info(&recommendations);
-			println!("Time taken to compute recommendations: {:.3?}", elapsed_time);
-			println!("Created {} recommendations.", recommendations.concrete.len() + recommendations.abs.len());
-			return;
-			output_recommendations(&output, "file.txt")
-				.unwrap_or_else(|e| println!("Error writing recommendations to file: {}", e));
-			println!("Recommendations written to file.");
-		}
-		Err(e) => println!("Error reading record file:\n	{}", e),
-	}
-} 
+    match create_data_directory() {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error creating data directory: {}", e);
+            return;
+        }
+    }
+    let parameters = input_parsing::get_input_parameters_from_user();
+    let start_time = Instant::now();
+    println!("Reading file");
+    let record = read_file_record(parameters.record_file);
+    match record {
+        Ok(record) => {
+            println!("Generating recommendations");
+            let recommendations =
+                compute_recommendations_from_record(&record, parameters.max_chain_size);
+            let elapsed_time = start_time.elapsed();
+            let output = create_sorted_info(&recommendations);
+            println!(
+                "Time taken to compute recommendations: {:.3?}",
+                elapsed_time
+            );
+            println!(
+                "Created {} recommendations.",
+                recommendations.concrete.len() + recommendations.abs.len()
+            );
+            return;
+            output_recommendations(&output, "file.txt")
+                .unwrap_or_else(|e| println!("Error writing recommendations to file: {}", e));
+            println!("Recommendations written to file.");
+        }
+        Err(e) => println!("Error reading record file:\n	{}", e),
+    }
+}
