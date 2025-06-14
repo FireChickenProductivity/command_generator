47dbdd399f1f9a05ed74f732a8c9019f47ddfc39
Fire Chicken <firechickenproductivity@gmail.com>
2025-06-14 14:08:27 -0600

    Draft thread pool

--- src/pool.rs
+++ src/pool.rs
@@ -1,38 +1,50 @@
-use std::thread;
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 struct Worker {
-	id: usize,
-	thread: thread::JoinHandle<()>,
+    id: usize,
+    thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
-	fn new(id: usize) -> Self {
-		let thread = thread::spawn(move || {
-			
-		});
-		Self {
-			id,
-			thread,
-		}
-	}
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} received a job", id);
+                job();
+            }
+        });
+        Self { id, thread }
+    }
 }
 
 pub struct ThreadPool {
-	workers: Vec<Worker>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
-	pub fn new(size: usize) -> Self {
-		let mut workers = Vec::with_capacity(size);
-		for id in 0..size {
-			workers.push(Worker::new(id));
-		}
-		Self {
-			workers
-		}
-	}
+    pub fn new(size: usize) -> Self {
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
 
-	pub fn execute<F>(&self, _f: F) where F: FnOnce() + Send + 'static {
-		// Placeholder for thread pool execution logic
-	}
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        Self { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
 }
